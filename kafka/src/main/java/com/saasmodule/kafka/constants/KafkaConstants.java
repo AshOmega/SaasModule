a8a450b436e62e4d262ce507bd1dfd8d31ad7ace
karollil <karollil@adobe.com>
2024-02-29 19:59:01 +0530

    KafkaController to push message for tryouts

---
+++ kafka/src/main/java/com/saasmodule/kafka/constants/KafkaConstants.java
@@ -0,0 +1,8 @@
+package com.saasmodule.kafka.constants;
+
+public class KafkaConstants {
+
+  public static String LATEST = "latest";
+  public static String EARLIEST = "earliest";
+  public static String SUCCESS = "success";
+}
